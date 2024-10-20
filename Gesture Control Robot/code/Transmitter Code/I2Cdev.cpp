4d41467233fd2938cccbea939bcce21aa99aba9c
Srijan_Ghimire <55309772+srijan101@users.noreply.github.com>
2024-10-20 21:48:20 +0700

    Update I2Cdev.cpp

--- Gesture Control Robot/code/Transmitter Code/I2Cdev.cpp
+++ Gesture Control Robot/code/Transmitter Code/I2Cdev.cpp
@@ -1,47 +1,3 @@
-// I2Cdev library collection - Main I2C device class
-// Abstracts bit and byte I2C R/W functions into a convenient class
-// 2013-06-05 by Jeff Rowberg <jeff@rowberg.net>
-//
-// Changelog:
-//      2013-05-06 - add Francesco Ferrara's Fastwire v0.24 implementation with small modifications
-//      2013-05-05 - fix issue with writing bit values to words (Sasquatch/Farzanegan)
-//      2012-06-09 - fix major issue with reading > 32 bytes at a time with Arduino Wire
-//                 - add compiler warnings when using outdated or IDE or limited I2Cdev implementation
-//      2011-11-01 - fix write*Bits mask calculation (thanks sasquatch @ Arduino forums)
-//      2011-10-03 - added automatic Arduino version detection for ease of use
-//      2011-10-02 - added Gene Knight's NBWire TwoWire class implementation with small modifications
-//      2011-08-31 - added support for Arduino 1.0 Wire library (methods are different from 0.x)
-//      2011-08-03 - added optional timeout parameter to read* methods to easily change from default
-//      2011-08-02 - added support for 16-bit registers
-//                 - fixed incorrect Doxygen comments on some methods
-//                 - added timeout value for read operations (thanks mem @ Arduino forums)
-//      2011-07-30 - changed read/write function structures to return success or byte counts
-//                 - made all methods static for multi-device memory savings
-//      2011-07-28 - initial release
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2013 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
 
 #include "I2Cdev.h"
 
@@ -49,22 +5,11 @@ THE SOFTWARE.
 
     #ifdef I2CDEV_IMPLEMENTATION_WARNINGS
         #if ARDUINO < 100
-            #warning Using outdated Arduino IDE with Wire library is functionally limiting.
-            #warning Arduino IDE v1.6.5+ with I2Cdev Fastwire implementation is recommended.
-            #warning This I2Cdev implementation does not support:
-            #warning - Repeated starts conditions
-            #warning - Timeout detection (some Wire requests block forever)
+
         #elif ARDUINO == 100
-            #warning Using outdated Arduino IDE with Wire library is functionally limiting.
-            #warning Arduino IDE v1.6.5+ with I2Cdev Fastwire implementation is recommended.
-            #warning This I2Cdev implementation does not support:
-            #warning - Repeated starts conditions
-            #warning - Timeout detection (some Wire requests block forever)
+
         #elif ARDUINO > 100
-            /*#warning Using current Arduino IDE with Wire library is functionally limiting.
-            #warning Arduino IDE v1.6.5+ with I2CDEV_BUILTIN_FASTWIRE implementation is recommended.
-            #warning This I2Cdev implementation does not support:
-            #warning - Timeout detection (some Wire requests block forever)*/
+
         #endif
     #endif
 
@@ -107,14 +52,7 @@ int8_t I2Cdev::readBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t
     return count;
 }
 
-/** Read a single bit from a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to read from
- * @param bitNum Bit position to read (0-15)
- * @param data Container for single bit value
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Status of read operation (true = success)
- */
+
 int8_t I2Cdev::readBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_t *data, uint16_t timeout) {
     uint16_t b;
     uint8_t count = readWord(devAddr, regAddr, &b, timeout);
@@ -122,15 +60,7 @@ int8_t I2Cdev::readBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16
     return count;
 }
 
-/** Read multiple bits from an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to read from
- * @param bitStart First bit position to read (0-7)
- * @param length Number of bits to read (not more than 8)
- * @param data Container for right-aligned value (i.e. '101' read from any bitStart position will equal 0x05)
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Status of read operation (true = success)
- */
+
 int8_t I2Cdev::readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t *data, uint16_t timeout) {
     // 01101001 read byte
     // 76543210 bit numbers
@@ -147,15 +77,7 @@ int8_t I2Cdev::readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint
     return count;
 }
 
-/** Read multiple bits from a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to read from
- * @param bitStart First bit position to read (0-15)
- * @param length Number of bits to read (not more than 16)
- * @param data Container for right-aligned value (i.e. '101' read from any bitStart position will equal 0x05)
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Status of read operation (1 = success, 0 = failure, -1 = timeout)
- */
+
 int8_t I2Cdev::readBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint16_t *data, uint16_t timeout) {
     // 1101011001101001 read byte
     // fedcba9876543210 bit numbers
@@ -173,36 +95,17 @@ int8_t I2Cdev::readBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uin
     return count;
 }
 
-/** Read single byte from an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to read from
- * @param data Container for byte value read from device
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Status of read operation (true = success)
- */
+
 int8_t I2Cdev::readByte(uint8_t devAddr, uint8_t regAddr, uint8_t *data, uint16_t timeout) {
     return readBytes(devAddr, regAddr, 1, data, timeout);
 }
 
-/** Read single word from a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to read from
- * @param data Container for word value read from device
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Status of read operation (true = success)
- */
+
 int8_t I2Cdev::readWord(uint8_t devAddr, uint8_t regAddr, uint16_t *data, uint16_t timeout) {
     return readWords(devAddr, regAddr, 1, data, timeout);
 }
 
-/** Read multiple bytes from an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr First register regAddr to read from
- * @param length Number of bytes to read
- * @param data Buffer to store read data in
- * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
- * @return Number of bytes read (-1 indicates failure)
- */
+
 int8_t I2Cdev::readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data, uint16_t timeout) {
     #ifdef I2CDEV_SERIAL_DEBUG
         Serial.print("I2C (0x");
@@ -481,13 +384,7 @@ bool I2Cdev::writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t 
     return writeByte(devAddr, regAddr, b);
 }
 
-/** write a single bit in a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to write to
- * @param bitNum Bit position to write (0-15)
- * @param value New bit value to write
- * @return Status of operation (true = success)
- */
+
 bool I2Cdev::writeBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_t data) {
     uint16_t w;
     readWord(devAddr, regAddr, &w);
@@ -495,14 +392,6 @@ bool I2Cdev::writeBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_
     return writeWord(devAddr, regAddr, w);
 }
 
-/** Write multiple bits in an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to write to
- * @param bitStart First bit position to write (0-7)
- * @param length Number of bits to write (not more than 8)
- * @param data Right-aligned value to write
- * @return Status of operation (true = success)
- */
 bool I2Cdev::writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
     //      010 value to write
     // 76543210 bit numbers
@@ -524,14 +413,7 @@ bool I2Cdev::writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8
     }
 }
 
-/** Write multiple bits in a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register regAddr to write to
- * @param bitStart First bit position to write (0-15)
- * @param length Number of bits to write (not more than 16)
- * @param data Right-aligned value to write
- * @return Status of operation (true = success)
- */
+
 bool I2Cdev::writeBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint16_t data) {
     //              010 value to write
     // fedcba9876543210 bit numbers
@@ -553,33 +435,17 @@ bool I2Cdev::writeBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint
     }
 }
 
-/** Write single byte to an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register address to write to
- * @param data New byte value to write
- * @return Status of operation (true = success)
- */
+
 bool I2Cdev::writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data) {
     return writeBytes(devAddr, regAddr, 1, &data);
 }
 
-/** Write single word to a 16-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr Register address to write to
- * @param data New word value to write
- * @return Status of operation (true = success)
- */
+
 bool I2Cdev::writeWord(uint8_t devAddr, uint8_t regAddr, uint16_t data) {
     return writeWords(devAddr, regAddr, 1, &data);
 }
 
-/** Write multiple bytes to an 8-bit device register.
- * @param devAddr I2C slave device address
- * @param regAddr First register address to write to
- * @param length Number of bytes to write
- * @param data Buffer to copy new data from
- * @return Status of operation (true = success)
- */
+
 bool I2Cdev::writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t* data) {
     #ifdef I2CDEV_SERIAL_DEBUG
         Serial.print("I2C (0x");
@@ -700,16 +566,6 @@ uint16_t I2Cdev::readTimeout = I2CDEV_DEFAULT_READ_TIMEOUT;
     // ferrara[at]libero[point]it
     //////////////////////
 
-    /*
-    FastWire
-    - 0.24 added stop
-    - 0.23 added reset
-
-     This is a library to help faster programs to read I2C devices.
-     Copyright(C) 2012 Francesco Ferrara
-     occhiobello at gmail dot com
-     [used by Jeff Rowberg for I2Cdevlib with permission]
-     */
 
     boolean Fastwire::waitInt() {
         int l = 250;
@@ -887,45 +743,8 @@ uint16_t I2Cdev::readTimeout = I2CDEV_DEFAULT_READ_TIMEOUT;
         if (!waitInt()) return 1;
         return 0;
     }
-#endif
 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_NBWIRE
-    // NBWire implementation based heavily on code by Gene Knight <Gene@Telobot.com>
-    // Originally posted on the Arduino forum at http://arduino.cc/forum/index.php/topic,70705.0.html
-    // Originally offered to the i2cdevlib project at http://arduino.cc/forum/index.php/topic,68210.30.html
 
-    /*
-    call this version 1.0
-    
-    Offhand, the only funky part that I can think of is in nbrequestFrom, where the buffer
-    length and index are set *before* the data is actually read. The problem is that these
-    are variables local to the TwoWire object, and by the time we actually have read the
-    data, and know what the length actually is, we have no simple access to the object's 
-    variables. The actual bytes read *is* given to the callback function, though.
-    
-    The ISR code for a slave receiver is commented out. I don't have that setup, and can't
-    verify it at this time. Save it for 2.0!
-    
-    The handling of the read and write processes here is much like in the demo sketch code: 
-    the process is broken down into sequential functions, where each registers the next as a
-    callback, essentially.
-    
-    For example, for the Read process, twi_read00 just returns if TWI is not yet in a 
-    ready state. When there's another interrupt, and the interface *is* ready, then it
-    sets up the read, starts it, and registers twi_read01 as the function to call after
-    the *next* interrupt. twi_read01, then, just returns if the interface is still in a
-    "reading" state. When the reading is done, it copies the information to the buffer,
-    cleans up, and calls the user-requested callback function with the actual number of 
-    bytes read.
-    
-    The writing is similar.
-    
-    Questions, comments and problems can go to Gene@Telobot.com.
-    
-    Thumbs Up!
-    Gene Knight
-    
-    */
     
     uint8_t TwoWire::rxBuffer[NBWIRE_BUFFER_LENGTH];
     uint8_t TwoWire::rxBufferIndex = 0;
